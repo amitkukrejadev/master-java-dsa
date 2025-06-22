6e5c12c51b68248cf486507feb0212d1c132a318
Amit Kukreja <thenameisamitkukreja@gmail.com>
2025-06-22 17:36:33 +0530

    -

--- Problems/Problem10.java
+++ Problems/Problem10.java
@@ -9,6 +9,22 @@ public class Problem10 {
          * using wrapper class constants (e.g., Integer.MAX_VALUE)
          * • Print each value with a proper label and formatting
          */
+        int vr1 = Integer.MIN_VALUE;
+        int vr2 = Integer.MAX_VALUE;
+        System.out.println("Int Min " + vr1);
+        System.out.println("Int Max " + vr2);
+        byte vr3 = Byte.MAX_VALUE;
+        byte vr4 = Byte.MIN_VALUE;
+        System.out.println("Byte Min " + vr3);
+        System.out.println("Byte Max " + vr4);
+        float vr5 = Float.MAX_VALUE;
+        float vr6 = Float.MIN_VALUE;
+        System.out.println("Float Min " + vr5);
+        System.out.println("Float Max " + vr6);
+        double vr7 = Double.MAX_VALUE;
+        double vr8 = Double.MIN_VALUE;
+        System.out.println("Double Min " + vr7);
+        System.out.println("Double Max " + vr8);
     }
-    
+
 }
