3f19045246c7208be2837331bc121244e42208d8
Amit Kukreja <thenameisamitkukreja@gmail.com>
2025-10-29 22:01:35 +0530

    Uncomment package declaration in Main.java and add Boolean class with a simple boolean example

--- 01_Basics/Main.java
+++ 01_Basics/Main.java
@@ -1,4 +1,4 @@
-package Basics;
+// package Basics;
 
 class Main {
     Main() {
---
+++ 02_Datatypes/Boolean.java
@@ -0,0 +1,6 @@
+public class Boolean {
+    public static void main(String[] args) {
+        boolean isPassed = true;
+        System.out.println(isPassed);
+    }
+}
